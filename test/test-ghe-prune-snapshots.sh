7bc3274d80ead7a57ebeb225f7de5708dd6c78e4
Ryan Tomayko <ryan@github.com>
2014-07-29 13:46:21 -0700

    Prune incomplete snapshots before expired snapshots
    Avoids pruning all successful snapshots and leaving only failed ones
    if many failed runs happen in a row. We always want to keep
    successful snapshots around.

--- test/test-ghe-prune-snapshots.sh
+++ test/test-ghe-prune-snapshots.sh
@@ -12,6 +12,7 @@ mkdir -p "$GHE_DATA_DIR"
 
 # helper for generating dirs to clean up
 generate_prune_files() {
+  rm -rf "$GHE_DATA_DIR/*"
   prune_file_num=${1:-10}
   for i in $(seq 1 $prune_file_num); do
     mkdir -p "$GHE_DATA_DIR/$i"
@@ -28,7 +29,9 @@ generate_prune_files 3
 begin_test "ghe-prune-snapshots uses default when GHE_NUM_BACKUPS not set"
 (
     set -e
+    generate_prune_files 12
     ghe-prune-snapshots
+    [ $(ls -1d "$GHE_DATA_DIR"/[0-9]* | wc -l) -eq 2 ]
 )
 end_test
 
@@ -44,6 +47,8 @@ begin_test "ghe-prune-snapshots doesn't prune if threshold isn't reached"
 (
   set -e
 
+  generate_prune_files 5
+
   pre_num_files=$(file_count_no_current)
 
   GHE_NUM_SNAPSHOTS=5 ghe-prune-snapshots
@@ -58,6 +63,8 @@ begin_test "ghe-prune-snapshots prunes if threshold is reached"
 (
   set -e
 
+  generate_prune_files 4
+
   pre_num_files=$(file_count_no_current)
 
   GHE_NUM_SNAPSHOTS=2 ghe-prune-snapshots
@@ -65,6 +72,22 @@ begin_test "ghe-prune-snapshots prunes if threshold is reached"
   post_num_files=$(file_count_no_current)
 
   # make sure we have different number of files and right file is deleted
-  [ $pre_num_files -gt $post_num_files -a ! -f "$GHE_DATA_DIR/1" ]
+  [ $post_num_files -eq 3 -a ! -f "$GHE_DATA_DIR/1" -a ! -f "$GHE_DATA_DIR/2" ]
+)
+end_test
+
+
+begin_test "ghe-prune-snapshots prunes incomplete snapshots"
+(
+    set -e
+
+    generate_prune_files 5
+
+    touch "$GHE_DATA_DIR/4/incomplete"
+
+    GHE_NUM_SNAPSHOTS=5 ghe-prune-snapshots
+
+    [ $(file_count_no_current) -eq 4 ]
+    [ ! -d "$GHE_DATA_DIR/4" ]
 )
 end_test

ebca53e441dbbfef6c63d7ea6f93c971cf68a431
Ben Gollmer <jatoben@github.com>
2016-08-19 15:11:39 -0600

    Always store backup-utils version

--- test/test-ghe-backup.sh
+++ test/test-ghe-backup.sh
@@ -476,6 +476,21 @@ begin_test "ghe-backup with no leaked keys"
 
   # Make sure there are no leaked key messages
   ! ghe-backup -v | grep "Leaked key"
+)
+end_test
 
+begin_test "ghe-backup stores version when not run from a clone"
+(
+  set -e
+
+  # Make sure this doesn't exist
+  rm -f "$GHE_REMOTE_DATA_USER_DIR/common/backup-utils-version"
+
+  mv "$ROOTDIR/.git" "$ROOTDIR/.gittmp"
+  ghe-backup
+  mv "$ROOTDIR/.gittmp" "$ROOTDIR/.git"
+
+  # verify that ghe-backup wrote its version information to the host
+  [ -f "$GHE_REMOTE_DATA_USER_DIR/common/backup-utils-version" ]
 )
 end_test

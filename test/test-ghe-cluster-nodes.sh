ad53f75b7b6326a133a9dd4a3d4f8d126baaf3ee
Avinash Sridhar <sridharavinash@github.com>
2017-03-02 12:43:24 -0500

    Forgot to add the test files :facepalm"

---
+++ test/test-ghe-cluster-nodes.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# ghe-cluster-nodes command tests
+
+# Bring in testlib
+. $(dirname "$0")/testlib.sh
+# Setup backup snapshot data dir and remote repositories dir locations to use
+# the per-test temp space.
+GHE_DATA_DIR="$TRASHDIR/data"
+GHE_REMOTE_DATA_DIR="$TRASHDIR/remote"
+export GHE_DATA_DIR GHE_REMOTE_DATA_DIR
+# Create a uuid file
+mkdir -p "$GHE_REMOTE_DATA_USER_DIR/common"
+echo "fake uuids" > "$GHE_REMOTE_DATA_USER_DIR/common/uuid"
+
+# Source in config file for hostname
+. "$GHE_BACKUP_CONFIG"
+
+begin_test "ghe-cluster-nodes should return both uuids for git-server"
+(
+    set -e
+
+    output="$(ghe-cluster-nodes "$GHE_HOSTNAME" "git-server")"
+    echo "$output"
+    [ "git-server-05cbcd42-f519-11e6-b6c9-002bd51dfa77 git-server-08d94884-f519-11e6-88a1-0063a7c33551 " = "$output" ]
+)
+end_test
+
+begin_test "ghe-cluster-nodes should return only one uuid for web-server"
+(
+    set -e
+
+    output="$(ghe-cluster-nodes "$GHE_HOSTNAME" "web-server")"
+    echo "$output"
+    [ "web-server-05cbcd42-f519-11e6-b6c9-002bd51dfa77 " = "$output" ]
+)
+end_test

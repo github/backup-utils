ffe537cf59d67cf3ddd2ba2af05bdc3b5618b769
Colin Seymour <colin@github.com>
2018-01-17 17:17:45 +0000

    Remove tests for old repo layout
    ... and merge nw tests into remaining tests.

--- test/test-ghe-backup-repositories-rsync.sh
+++ test/test-ghe-backup-repositories-rsync.sh
@@ -8,13 +8,24 @@
 mkdir -p "$GHE_DATA_DIR" "$GHE_REMOTE_DATA_USER_DIR/repositories"
 mkdir -p "$TRASHDIR/hooks"
 
-# Create some test repositories in the remote repositories dir
+# Run under the remote repositories directory
 cd "$GHE_REMOTE_DATA_USER_DIR/repositories"
-mkdir alice bob
-mkdir alice/repo1.git alice/repo2.git bob/repo3.git
+
+# Create some test repositories in the remote repositories dir
+repo1="0/nw/01/aa/3f/1234/1234.git"
+repo2="0/nw/01/aa/3f/1234/1235.git"
+repo3="1/nw/23/bb/4c/2345/2345.git"
+mkdir -p "$repo1" "$repo2" "$repo3"
+
+wiki1="0/nw/01/aa/3f/1234/1234.wiki.git"
+mkdir -p "$wiki1"
+
+gist1="0/01/aa/3f/gist/93069ad4c391b6203f183e147d52a97a.git"
+gist2="1/23/bb/4c/gist/1234.git"
+mkdir -p "$gist1" "$gist2"
 
 # Initialize test repositories with a fake commit
-for repo in */*.git; do
+for repo in $(find . -type d -name '*.git' -prune); do
     git init -q --bare "$repo"
     git --git-dir="$repo" --work-tree=. commit -q --allow-empty -m 'test commit'
     rm -rf "$repo/hooks"
@@ -22,15 +33,15 @@ for repo in */*.git; do
 done
 
 # Generate a packed-refs file in repo1
-git --git-dir="alice/repo1.git" pack-refs
+git --git-dir="$repo1" pack-refs
 
 # Generate a pack in repo2
-git --git-dir="alice/repo2.git" repack -q
+git --git-dir="$repo2" repack -q
 
 # Add some fake svn data to repo3
-echo "fake svn history data" > bob/repo3.git/svn.history.msgpack
-mkdir bob/repo3.git/svn_data
-echo "fake property history data" > bob/repo3.git/svn_data/property_history.msgpack
+echo "fake svn history data" > "$repo3/svn.history.msgpack"
+mkdir "$repo3/svn_data"
+echo "fake property history data" > "$repo3/svn_data/property_history.msgpack"
 
 begin_test "ghe-backup-repositories-rsync first snapshot"
 (
@@ -47,14 +58,14 @@ begin_test "ghe-backup-repositories-rsync first snapshot"
     [ -d "$GHE_DATA_DIR/1/repositories" ]
 
     # check that packed-refs file was transferred
-    [ -f "$GHE_DATA_DIR/1/repositories/alice/repo1.git/packed-refs" ]
+    [ -f "$GHE_DATA_DIR/1/repositories/$repo1/packed-refs" ]
 
     # check that a pack file was transferred
-    [ -f "$GHE_DATA_DIR"/1/repositories/alice/repo2.git/objects/pack/*.pack ]
+    [ -f "$GHE_DATA_DIR"/1/repositories/$repo2/objects/pack/*.pack ]
 
     # check that svn data was transferred
-    [ -f "$GHE_DATA_DIR"/1/repositories/bob/repo3.git/svn.history.msgpack ]
-    [ -f "$GHE_DATA_DIR"/1/repositories/bob/repo3.git/svn_data/property_history.msgpack ]
+    [ -f "$GHE_DATA_DIR"/1/repositories/$repo3/svn.history.msgpack ]
+    [ -f "$GHE_DATA_DIR"/1/repositories/$repo3/svn_data/property_history.msgpack ]
 
     # verify all repository data was transferred
     diff -ru "$GHE_REMOTE_DATA_USER_DIR/repositories" "$GHE_DATA_DIR/1/repositories"
@@ -81,8 +92,8 @@ begin_test "ghe-backup-repositories-rsync subsequent snapshot"
     [ -d "$snapshot" ]
 
     # verify hard links used for existing files
-    inode1=$(ls -i "$GHE_DATA_DIR/1/repositories/alice/repo1.git/packed-refs" | awk '{ print $1; }')
-    inode2=$(ls -i "$GHE_DATA_DIR/2/repositories/alice/repo1.git/packed-refs" | awk '{ print $1; }')
+    inode1=$(ls -i "$GHE_DATA_DIR/1/repositories/$repo1/packed-refs" | awk '{ print $1; }')
+    inode2=$(ls -i "$GHE_DATA_DIR/2/repositories/$repo1/packed-refs" | awk '{ print $1; }')
     [ "$inode1" = "$inode2" ]
 
     # verify all repository data exists in the increment
@@ -99,10 +110,6 @@ begin_test "ghe-backup-repositories-rsync __special__ dirs"
     GHE_SNAPSHOT_TIMESTAMP=3
     export GHE_SNAPSHOT_TIMESTAMP
 
-    # create the excluded __nodeload_archives__ dir on the remote side
-    mkdir "$GHE_REMOTE_DATA_USER_DIR/repositories/__nodeload_archives__"
-    touch "$GHE_REMOTE_DATA_USER_DIR/repositories/__nodeload_archives__/something.tar.gz"
-
     # create the included __purgatory__ dir on the remote side
     mkdir -p "$GHE_REMOTE_DATA_USER_DIR/repositories/__purgatory__/123.git"
     git init --bare -q "$GHE_REMOTE_DATA_USER_DIR/repositories/__purgatory__/123.git"
@@ -115,9 +122,6 @@ begin_test "ghe-backup-repositories-rsync __special__ dirs"
     # check that repositories directory was created
     snapshot="$GHE_DATA_DIR/3/repositories"
     [ -d "$snapshot" ]
-
-    # check that excluded special dir was not transferred
-    [ ! -d "$snapshot/__nodeload_archives__" ]
 
     # check that included special dir was not transferred
     [ -d "$snapshot/__purgatory__" ]
@@ -139,7 +143,7 @@ begin_test "ghe-backup-repositories-rsync temp files"
     export GHE_SNAPSHOT_TIMESTAMP
 
     # create a tmp pack to emulate a pack being written to
-    touch "$GHE_REMOTE_DATA_USER_DIR/repositories/alice/repo1.git/objects/pack/tmp_pack_1234"
+    touch "$GHE_REMOTE_DATA_USER_DIR/repositories/$repo1/objects/pack/tmp_pack_1234"
 
     # run it
     ghe-backup-repositories-rsync
@@ -149,6 +153,6 @@ begin_test "ghe-backup-repositories-rsync temp files"
     [ -d "$snapshot" ]
 
     # check that tmp pack was not transferred
-    [ ! -d "$snapshot/alice/repo1.git/objects/pack/tmp_pack_1234" ]
+    [ ! -d "$snapshot/$repo1/objects/pack/tmp_pack_1234" ]
 )
 end_test

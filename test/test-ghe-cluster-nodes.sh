758d095b4cca9cc48202f0ab39bde8acc1819cf9
Colin Seymour <colin@github.com>
2018-01-18 17:20:44 +0000

    Add support for standalone nodes
    ... and update tests

--- test/test-ghe-cluster-nodes.sh
+++ test/test-ghe-cluster-nodes.sh
@@ -12,14 +12,28 @@ GHE_REMOTE_DATA_DIR="$TRASHDIR/remote"
 export GHE_DATA_DIR GHE_REMOTE_DATA_DIR
 # Create a uuid file
 mkdir -p "$GHE_REMOTE_DATA_USER_DIR/common"
-echo "fake uuids" > "$GHE_REMOTE_DATA_USER_DIR/common/uuid"
+echo "fake-uuid" > "$GHE_REMOTE_DATA_USER_DIR/common/uuid"
 
 begin_test "ghe-cluster-nodes should return both uuids for git-server"
 (
     set -e
+    setup_remote_cluster
 
     output="$(ghe-cluster-nodes "$GHE_HOSTNAME" "git-server")"
     echo "$output"
     [ "git-server-05cbcd42-f519-11e6-b6c9-002bd51dfa77 git-server-08d94884-f519-11e6-88a1-0063a7c33551 " = "$output" ]
+)
+end_test
+
+begin_test "ghe-cluster-nodes should return one uuid for a single node"
+(
+    set -e
+
+    # Ensure not a cluster
+    rm -rf "$GHE_REMOTE_ROOT_DIR/etc/github/cluster"
+
+    output="$(ghe-cluster-nodes "$GHE_HOSTNAME" "git-server")"
+    echo "$output"
+    [ "git-server-fake-uuid" = "$output" ]
 )
 end_test

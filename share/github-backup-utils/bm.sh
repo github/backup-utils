7e9b680b777ca932350b1cff2443e41878d7aa05
Colin Seymour <colin@github.com>
2018-04-14 10:50:15 +0100

    Quote array expansion

--- share/github-backup-utils/bm.sh
+++ share/github-backup-utils/bm.sh
@@ -48,7 +48,7 @@ bm_end() {
 
   local tend tstart total
   tend=$(date +%s)
-  tstart=$(eval "echo \$$(bm_desc_to_varname $@)_start")
+  tstart=$(eval "echo \$$(bm_desc_to_varname "$@")_start")
   total=$(($tend - $tstart))
 
   echo "$1 took ${total}s" >> $BM_FILE_PATH

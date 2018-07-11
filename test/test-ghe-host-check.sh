6b5f755a0889f0612bca9ed1c4d8a7a7be6551b5
Colin Seymour <colin@github.com>
2018-07-11 10:52:17 +0100

    Make ghe-host-check test less version-specific

--- test/test-ghe-host-check.sh
+++ test/test-ghe-host-check.sh
@@ -52,14 +52,18 @@ end_test
 begin_test "ghe-host-check detects unsupported GitHub Enterprise versions"
 (
   set -e
+  # shellcheck disable=SC2046 # Word splitting is required to populate the variables
+  read -r bu_version_major bu_version_minor _ <<<$(ghe_parse_version $BACKUP_UTILS_VERSION)
+
   ! GHE_TEST_REMOTE_VERSION=11.340.36 ghe-host-check
-  ! GHE_TEST_REMOTE_VERSION=2.10.0 ghe-host-check
-  GHE_TEST_REMOTE_VERSION=2.11.0 ghe-host-check
-  GHE_TEST_REMOTE_VERSION=2.12.0 ghe-host-check
-  GHE_TEST_REMOTE_VERSION=2.13.999 ghe-host-check
-  GHE_TEST_REMOTE_VERSION=2.13.999gm1 ghe-host-check
+  ! GHE_TEST_REMOTE_VERSION=2.$((bu_version_minor-3)).0 ghe-host-check
+  GHE_TEST_REMOTE_VERSION=2.$((bu_version_minor-2)).0 ghe-host-check
+  GHE_TEST_REMOTE_VERSION=$BACKUP_UTILS_VERSION ghe-host-check
+  GHE_TEST_REMOTE_VERSION=$BACKUP_UTILS_VERSION ghe-host-check
+  GHE_TEST_REMOTE_VERSION=$bu_version_major.$bu_version_minor.999 ghe-host-check
+  GHE_TEST_REMOTE_VERSION=$bu_version_major.$bu_version_minor.999gm1 ghe-host-check
   ! GHE_TEST_REMOTE_VERSION=2.9999.1521793591.performancetest ghe-host-check
-  GHE_TEST_REMOTE_VERSION=3.0.0 ghe-host-check
+  GHE_TEST_REMOTE_VERSION=$((bu_version_minor+1)).0.0 ghe-host-check
 )
 end_test
 

5248df4f5d5a5f3d679b6663e08f03a930219f83
Ben Blazely <taz@users.noreply.github.com>
2019-04-23 17:27:00 +1000

    fix test

--- test/test-ghe-restore.sh
+++ test/test-ghe-restore.sh
@@ -209,6 +209,8 @@ begin_test "ghe-restore with host arg and config value"
   GHE_BACKUP_CONFIG_TEMP="${GHE_BACKUP_CONFIG}.temp"
   cp "$GHE_BACKUP_CONFIG" "$GHE_BACKUP_CONFIG_TEMP"
   echo 'GHE_BACKUP_HOST="broken.config.restore.host"' >> "$GHE_BACKUP_CONFIG_TEMP"
+  GHE_BACKUP_CONFIG="$GHE_BACKUP_CONFIG_TEST"
+  export GHE_BACKUP_CONFIG
 
   # run it
   output="$(ghe-restore -f localhost)" || false

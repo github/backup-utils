c07afd9d43bd83486749bc8184dd6de58c8820f5
Evgenii Khramkov <ewgeniux@gmail.com>
2020-04-13 23:04:52 -0700

    fix tests by passing "--copy-dirlinks" argument

--- test/test-ghe-restore-parallel.sh
+++ test/test-ghe-restore-parallel.sh
@@ -3,6 +3,7 @@
 set -e
 
 export GHE_PARALLEL_ENABLED=yes
+export GHE_EXTRA_RSYNC_OPTS="--copy-dirlinks" # handle symlink directories for tests
 
 TESTS_DIR="$PWD/$(dirname "$0")"
 # shellcheck source=test/test-ghe-restore.sh

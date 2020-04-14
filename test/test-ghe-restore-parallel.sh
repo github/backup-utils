c6daeaf6acb92038741aa6c87e1a504ecd17d2a2
Evgenii Khramkov <ewgeniux@gmail.com>
2020-04-13 23:14:03 -0700

    more detailed comment

--- test/test-ghe-restore-parallel.sh
+++ test/test-ghe-restore-parallel.sh
@@ -2,11 +2,13 @@
 # ghe-restore command tests run in parallel
 set -e
 
-# use tempdir to fix rsync file issues in parallel execution
-tempdir=$(mktemp -d -t backup-utils-restore-temp-XXXXXX)
-
 export GHE_PARALLEL_ENABLED=yes
-export GHE_EXTRA_RSYNC_OPTS="--copy-dirlinks --temp-dir=$tempdir" # handle symlink directories for tests
+
+# use temp dir to fix rsync file issues in parallel execution:
+# we are imitating remote server by local files, and running rsync in parallel may cause
+# race conditions when two processes writing to same folder
+tempdir=$(mktemp -d -t backup-utils-restore-temp-XXXXXX)
+export GHE_EXTRA_RSYNC_OPTS="--copy-dirlinks --temp-dir=$tempdir"
 
 TESTS_DIR="$PWD/$(dirname "$0")"
 # shellcheck source=test/test-ghe-restore.sh

14d3aacfe3f0b7a90aed2e03c6b81fd7dde5af2b
Evgenii Khramkov <ewgeniux@gmail.com>
2020-04-13 23:10:14 -0700

    use temp rsync directory for parallel restore test

--- test/test-ghe-restore-parallel.sh
+++ test/test-ghe-restore-parallel.sh
@@ -2,8 +2,11 @@
 # ghe-restore command tests run in parallel
 set -e
 
+# use tempdir to fix rsync file issues in parallel execution
+tempdir=$(mktemp -d -t backup-utils-restore-temp-XXXXXX)
+
 export GHE_PARALLEL_ENABLED=yes
-export GHE_EXTRA_RSYNC_OPTS="--copy-dirlinks" # handle symlink directories for tests
+export GHE_EXTRA_RSYNC_OPTS="--copy-dirlinks --temp-dir=$tempdir" # handle symlink directories for tests
 
 TESTS_DIR="$PWD/$(dirname "$0")"
 # shellcheck source=test/test-ghe-restore.sh

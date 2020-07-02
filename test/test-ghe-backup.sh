2521ebc6db2f44acb235cbb989d09d9a9ffd30bf
David Hadka <dhadka@github.com>
2020-07-02 17:20:51 +0000

    Fix tests

--- test/test-ghe-backup.sh
+++ test/test-ghe-backup.sh
@@ -407,7 +407,7 @@ begin_test "ghe-backup takes backup of Actions files"
   enable_actions
 
   output=$(ghe-backup -v)
-  echo $output | grep "Transferring Actions files"
+  echo $output | grep "Transferring actions files to"
   
   diff -ru "$GHE_REMOTE_DATA_USER_DIR/actions" "$GHE_DATA_DIR/current/actions"
 )
--- test/test-ghe-restore.sh
+++ test/test-ghe-restore.sh
@@ -317,7 +317,7 @@ begin_test "ghe-restore with Actions files"
 
   output=$(ghe-restore -v -f localhost 2>&1)
 
-  echo "$output" | grep -q "Transferring Actions files to"
+  echo "$output" | grep -q "Transferring actions files to"
 
   diff -ru "$GHE_REMOTE_DATA_USER_DIR/actions" "$GHE_DATA_DIR/current/actions"
 )

aadb8b955d3e9636bebb41397a716435f1be1668
Anupam Kumar <atirikt@github.com>
2022-07-28 17:39:56 +0000

    restore 1

--- test/test-ghe-restore.sh
+++ test/test-ghe-restore.sh
@@ -309,6 +309,32 @@ begin_test "ghe-restore invokes ghe-import-mssql"
 )
 end_test
 
+begin_test "ghe-restore with Kredz settings"
+(
+  set -e
+  rm -rf "$GHE_REMOTE_ROOT_DIR"
+  setup_remote_metadata
+  enable_actions
+
+  required_files=(
+    "kredz-credz-hmac"
+  )
+
+  for file in "${required_files[@]}"; do
+    echo "foo" > "$GHE_DATA_DIR/current/$file"
+  done
+
+  ghe-restore -v -f localhost
+  required_secrets=(
+    "secrets.kredz.credz-hmac-secret"
+  )
+  
+  for secret in "${required_secrets[@]}"; do
+    [ "$(ghe-ssh "$GHE_HOSTNAME" -- ghe-config "$secret")" = "foo" ]
+  done
+)
+end_test
+
 begin_test "ghe-restore with Actions settings"
 (
   set -e
@@ -347,7 +373,6 @@ begin_test "ghe-restore with Actions settings"
     "actions-launch-azp-app-cert"
     "actions-launch-app-app-private-key"
 
-    "kredz-credz-hmac"
   )
 
   for file in "${required_files[@]}"; do
@@ -389,7 +414,6 @@ begin_test "ghe-restore with Actions settings"
     "secrets.launch.azp-app-cert"
     "secrets.launch.azp-app-private-key"
 
-    "secrets.kredz.credz-hmac-secret"
   )
 
   for secret in "${required_secrets[@]}"; do

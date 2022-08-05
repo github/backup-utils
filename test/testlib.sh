c9c5de6b2a606141fd643498f763f66433ec1ccb
Luca Cavallin <lucavallin@github.com>
2022-08-05 16:47:12 +0200

    empty settings.json before using git config

--- test/testlib.sh
+++ test/testlib.sh
@@ -598,5 +598,7 @@ cleanup_moreutils_parallel() {
 # setup_actions_enabled_in_settings_json writes settings for the Actions app to settings.json
 # it accepts true or false as first argument to enable or disable actions in settings.json
 setup_actions_enabled_settings_for_restore() {
+  # Empty the file, it now contains "fake ghe-export-settings data"
+  > "$GHE_DATA_DIR/1/settings.json"
   git config -f "$GHE_DATA_DIR/1/settings.json" --bool app.actions.enabled $1
 }

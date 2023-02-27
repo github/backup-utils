7d117eda9fae91d31b39672d44e061f9395f554a
Chuck Pathanjali <chuckp22@github.com>
2023-02-27 17:11:27 +0000

    Updated hostcheck to use rsync

---
+++ share/github-backup-utils/ghe-rsync-size.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+# get-rsync-size.sh Get the total size of dir-files to be transfered using rsync --link-dest
+#
+# Example:
+#   transfer_size repositories /dest_dir
+#
+# Sample output:
+#   Total transferred file size: 80 bytes
+
+# Bring in the backup configuration
+# shellcheck source=share/github-backup-utils/ghe-backup-config
+. "$(dirname "${BASH_SOURCE[0]}")/ghe-backup-config"
+
+# Location of last good backup for rsync --link-dest
+backup_current="$GHE_DATA_DIR/current/"
+
+# If we have a previous increment, avoid using those unchanged files using --link-dest support.
+if [ -d "$backup_current" ]; then
+  link_dest="--link-dest=${GHE_DATA_DIR}/current"
+fi
+
+transfer_size()
+{
+  local backup_data=$1
+  local dir=/data/user/$1/
+  local dest_dir=$2/
+
+  # Define user for rsync-path
+  case "$backup_data" in
+  "repositories" | "pages")
+    user="git"
+    ;;
+  "storage")
+    user="alambic"
+    ;;
+  "elasticsearch")
+    user="elasticsearch"
+    ;;
+  "mysql")
+    user="mysql"
+    ;;
+  "mssql")
+    user="mssql"
+    ;;
+  "minio")
+    user="minio"
+    ;;
+  *)
+    echo "Unknown user: $backup_data"
+    exit 1
+    ;;
+  esac
+
+#  local total_file_size=$(ghe-ssh "$GHE_HOSTNAME" sudo rsync -arvn $link_dest/$1 --stats "$dir" "$dest_dir" | grep "Total transferred file size" | sed 's/.*size: //; s/,//')
+  local total_file_size=$(ghe-rsync -arn --stats \
+      -e "ssh -q $GHE_EXTRA_SSH_OPTS -p 122 -l admin" \
+      --rsync-path="sudo -u $user rsync" \
+      $link_dest/$1 \
+      --ignore-missing-args \
+      "$GHE_HOSTNAME:$GHE_REMOTE_DATA_USER_DIR/user/$1/" \
+      "$dest_dir" | grep "Total transferred file size" | sed 's/.*size: //; s/,//g')
+  echo "$total_file_size" 
+}

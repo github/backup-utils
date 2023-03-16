6f647e8229b863ac3dfa8d70b9b1d88aa18ec056
Quinn Murphy <gamefiend@github.com>
2023-03-16 19:30:53 +0000

    troubleshooting output

--- test/testlib.sh
+++ test/testlib.sh
@@ -494,6 +494,7 @@ verify_all_restored_data() {
   if ! $SKIP_MYSQL; then
     grep -q "fake ghe-export-mysql data" "$TRASHDIR/restore-out"
   fi
+  cat "$TRASHDIR/restore-out" 
   grep -q "fake ghe-export-redis data" "$TRASHDIR/restore-out"
   grep -q "fake ghe-export-authorized-keys data" "$TRASHDIR/restore-out"
 

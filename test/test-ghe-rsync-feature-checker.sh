3d2af0a5ad9982a1df77f807235097afd63db45b
ghe-admin <xn4p4lm@github.com>
2023-05-03 13:38:49 -0400

    Updated test to standard convention

--- test/test-ghe-rsync-feature-checker.sh
+++ test/test-ghe-rsync-feature-checker.sh
@@ -6,9 +6,38 @@ TESTS_DIR="$PWD/$(dirname "$0")"
 # shellcheck source=test/testlib.sh
 . "$TESTS_DIR/testlib.sh"
 
+begin_test "ghe-rsync-feature-checker for know command `--help`"
+(
+  set -e
 
-# Test ghe-rsync-feature-checker command
-ghe-rsync-feature-checker "--help" | grep -q "true"
-ghe-rsync-feature-checker "help" | grep -q "true"
-ghe-rsync-feature-checker "--ignore-missing-args" | grep -q "false"
-ghe-rsync-feature-checker "ignore-missing-args" | grep -q "false"
+  # Test ghe-rsync-feature-checker command
+    ghe-rsync-feature-checker "--help" | grep -q "true"
+)
+end_test
+
+begin_test "ghe-rsync-feature-checker for know command `help`"
+(
+    set -e
+
+    # Test ghe-rsync-feature-checker command
+    ghe-rsync-feature-checker "help" | grep -q "true"
+)
+end_test
+
+begin_test "ghe-rsync-feature-checker for known unsupported command `--ignore-missing-args`"
+(
+    set -e
+
+    # Test ghe-rsync-feature-checker command
+    ghe-rsync-feature-checker "--ignore-missing-args" | grep -q "false"
+)
+end_test
+
+begin_test "ghe-rsync-feature-checker for known unsupported command `ignore-missing-args`"
+(
+    set -e
+
+    # Test ghe-rsync-feature-checker command
+    ghe-rsync-feature-checker "ignore-missing-args" | grep -q "false"
+)
+end_test

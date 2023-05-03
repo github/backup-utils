5f7d0ccc539a7bb4f0b777d54b0e96ee61e3cdc5
ghe-admin <xn4p4lm@github.com>
2023-05-03 14:39:11 -0400

    Feature checker updated - Now using `grep -E "\b$rsync_command\b"` for explicit checking

--- share/github-backup-utils/ghe-rsync-feature-checker.sh
+++ share/github-backup-utils/ghe-rsync-feature-checker.sh
@@ -9,11 +9,11 @@ set -o pipefail
 # set the variable from the first argument
 rsync_command="$1"
 
-# strip -- from the passed rsync command
-rsync_command="${rsync_command/--/}"
+# strip - and -- from the passed rsync command
+rsync_command="${rsync_command/-/}"
 
 # check if the passed rsync command is supported by the current version of rsync
-if rsync -h | grep "$rsync_command" >/dev/null 2>&1; then
+if rsync -h | grep -E "\b$rsync_command\b" >/dev/null 2>&1; then
   echo "true"
 else
   echo "false"

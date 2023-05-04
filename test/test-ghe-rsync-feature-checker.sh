93e19c25205a228a6e5bc8b8315ef61800d8ff1c
ghe-admin <xn4p4lm@github.com>
2023-05-04 09:48:40 -0400

    fixed issue with tests - Was missing `.sh` in the `ghe-rsync-feature-checker calls`

--- test/test-ghe-rsync-feature-checker.sh
+++ test/test-ghe-rsync-feature-checker.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
-# ghe-rsync-feature-checker command tests
+# ghe-rsync-feature-checker.sh  command tests
 
 TESTS_DIR="$PWD/$(dirname "$0")"
 # Bring in testlib.
 # shellcheck source=test/testlib.sh
 . "$TESTS_DIR/testlib.sh"
 
-begin_test "ghe-rsync-feature-checker for know command --help"
+begin_test "ghe-rsync-feature-checker.sh  for know command --help"
 (
   set -e
 
-  # Test ghe-rsync-feature-checker command
-    ghe-rsync-feature-checker "--help" | grep -q "true"
+  # Test ghe-rsync-feature-checker.sh  command
+    ghe-rsync-feature-checker.sh --help | grep -q "true"
 )
 end_test
 
-begin_test "ghe-rsync-feature-checker for know command help"
+begin_test "ghe-rsync-feature-checker.sh  for know command help"
 (
     set -e
 
-    # Test ghe-rsync-feature-checker command
-    ghe-rsync-feature-checker "help" | grep -q "true"
+    # Test ghe-rsync-feature-checker.sh  command
+    ghe-rsync-feature-checker.sh  "help" | grep -q "true"
 )
 end_test
 
-begin_test "ghe-rsync-feature-checker for known unsupported command --ignore-missing-args"
+begin_test "ghe-rsync-feature-checker.sh  for known unsupported command --ignore-missing-args"
 (
     set -e
 
-    # Test ghe-rsync-feature-checker command
-    ghe-rsync-feature-checker "--ignore-missing-args" | grep -q "false"
+    # Test ghe-rsync-feature-checker.sh  command
+    ghe-rsync-feature-checker.sh  "--ignore-missing-args" | grep -q "false"
 )
 end_test
 
-begin_test "ghe-rsync-feature-checker for known unsupported command ignore-missing-args"
+begin_test "ghe-rsync-feature-checker.sh  for known unsupported command ignore-missing-args"
 (
     set -e
 
-    # Test ghe-rsync-feature-checker command
-    ghe-rsync-feature-checker "ignore-missing-args" | grep -q "false"
+    # Test ghe-rsync-feature-checker.sh  command
+    ghe-rsync-feature-checker.sh  "ignore-missing-args" | grep -q "false"
 )
 end_test

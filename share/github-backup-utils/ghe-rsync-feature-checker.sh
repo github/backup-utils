e3cc4d6999cd0be1a9e8297283c9791c11e92a57
ghe-admin <xn4p4lm@github.com>
2023-05-04 13:46:46 -0400

    Removed debug command from testing feature checker

--- share/github-backup-utils/ghe-rsync-feature-checker.sh
+++ share/github-backup-utils/ghe-rsync-feature-checker.sh
@@ -9,8 +9,6 @@ set -o pipefail
 # set the variable from the first argument and remove any leading dashes
 rsync_command=$(echo "$1" | sed -E 's/^-+//')
 
-echo "rsync_command: $rsync_command"
-
 # check if the passed rsync command is supported by the current version of rsync
 if rsync -h | grep -E "\B-+($rsync_command)\b" >/dev/null 2>&1; then
   echo "true"

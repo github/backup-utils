7814f409aafaec1926b85cd32e99da928054d930
timreimherr <timreimherr@users.noreply.github.com>
2023-10-27 16:11:08 +0000

    99.99.99 release

--- test/test-ghe-backup-parallel.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# ghe-backup command tests run in parallel
-set -e
-
-export GHE_PARALLEL_ENABLED=yes
-
-TESTS_DIR="$PWD/$(dirname "$0")"
-# shellcheck source=test/test-ghe-backup.sh
-. "$TESTS_DIR/test-ghe-backup.sh"

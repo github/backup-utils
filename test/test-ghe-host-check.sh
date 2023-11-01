970cb37787700dd7590e4a4f38dd8aa27e30a77a
Chuck Pathanjali <chuckp22@github.com>
2023-11-01 21:17:42 +0000

    SC updates

--- test/test-ghe-host-check.sh
+++ test/test-ghe-host-check.sh
@@ -134,7 +134,7 @@ begin_test "ghe-host-check fails when encountering RO file-system"
   ghe-ssh "$GHE_HOSTNAME" -- 'chmod a-w -R "~/tmp"'
 
   # File creation fails for CLUSTER
-  ! FILE_TO_TEST="~/tmp/test" CLUSTER=true ghe-host-check
-  FILE_TO_TEST="~/tmp/test" CLUSTER=false ghe-host-check
+  ! FILE_TO_TEST="$HOME/tmp/test" CLUSTER=true ghe-host-check
+  FILE_TO_TEST="$HOME/tmp/test" CLUSTER=false ghe-host-check
 )
 end_test
